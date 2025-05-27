2e11bbec89d841857e97a00329301ecb2c66097f
SaketKhundia <104209657+saketkhundia@users.noreply.github.com>
2025-05-27 15:06:46 +0530

    Add files via upload

---
+++ CF_2044A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CF_2044A{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();  
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();  
+            System.out.println(n - 1);
+        }
+
+        sc.close();
+    }
+}
