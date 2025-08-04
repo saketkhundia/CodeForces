56c845a29aec44a7daa8f93d631a388c51dd1f06
SaketKhundia <104209657+saketkhundia@users.noreply.github.com>
2025-08-04 07:55:11 +0530

    Add files via upload

---
+++ CF_1608A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CF_1608A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            for (int i = 2; i <= n + 1; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
